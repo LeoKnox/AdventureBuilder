cdcc1743d819c56d7b73ca0cfac01f82fdf749fd
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-01 16:55:15 -0700

    Added web API
    added a web api for database.

---
+++ CharacterDataStorage/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CharacterDataStorage.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CharacterDataStorage/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CharacterDataStorage.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
